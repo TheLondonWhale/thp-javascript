fe8f2ca13d137a23a6475803a32143fdc1c2e9fa
TheLondonWhale <thomas.perus@espeme.com>
2018-09-27 13:44:31 +0200

    le js propre mais la pop up avant le site

--- Javascriptpourpopup.js
+++ Javascriptpourpopup.js
@@ -11,8 +11,10 @@
 
 	let resultat = confirm ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Allez sur xxxvidsxxx ?");
        
-        if (resultat = true) {window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    };
+        if (resultat === true) 
+        {
+        window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    	}
 
 
 
