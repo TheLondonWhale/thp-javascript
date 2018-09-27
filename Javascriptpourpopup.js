1c5d8d1a54eaebdc39b0a473b01684e7e6b72bdf
TheLondonWhale <thomas.perus@espeme.com>
2018-09-27 12:44:00 +0200

    HTML propre et début du JS

--- Javascriptpourpopup.js
+++ Javascriptpourpopup.js
@@ -1,11 +1,18 @@
 
-        
+        /*
         setTimeout(function()
         {
              confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
              var haut=(screen.height-hauteur)/2;
              var Gauche=(screen.width-largeur)/2;
 
-         }, 3000);
+         }, 3000); */
+
+
+         let resultat = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Allez sur xxxvidsxxx ?") 
+         { 
+         	if (true) {window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
+         };
+alert(resultat);
 
 
