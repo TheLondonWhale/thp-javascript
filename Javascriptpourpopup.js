854e29b5199add5d0205d2eae141af808dcd5436
TheLondonWhale <thomas.perus@espeme.com>
2018-09-27 15:12:30 +0200

    enfin fini

--- Javascriptpourpopup.js
+++ Javascriptpourpopup.js
@@ -1,22 +1,11 @@
 
-        /*
-        setTimeout(function()
-        {
-             confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
-             var haut=(screen.height-hauteur)/2;
-             var Gauche=(screen.width-largeur)/2;
-
-         }, 3000); */
-
-
-let resultat = confirm ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Allez sur xxxvidsxxx ?");
-
-function BigFunction(resultat)
+function BigFunction()
 {
+	let resultat = confirm ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please. Allez sur xxxvidsxxx ?");
 	if (resultat === true) 
     {
     window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
    	}
 }
 
-setTimeout(BigFunction(), 5000)
+setTimeout(BigFunction, 5000)
